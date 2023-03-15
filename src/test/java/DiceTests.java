60da5d65a925633971ee3a7ad243f4563ebc0b62
Duc Tram <dtram61@gmail.com>
2023-03-15 08:38:00 -0700

    Created two tests for dice class

---
+++ src/test/java/DiceTests.java
@@ -0,0 +1,33 @@
+import org.example.objects.Dice;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class DiceTests {
+
+    @Test
+    public void diceTest()
+    {
+        Dice dice = new Dice(6, "blue");
+        assertEquals(6,dice.getSides());
+        assertEquals("blue",dice.getColor());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6})
+    public void validNumber(int number)
+    {
+       Dice dice = new Dice(6, "blue");
+
+       int roll = dice.roll();
+        assertTrue(roll >= 1 && roll <= 6);
+
+    }
+}
